9fc01807f1809e2fa9ddb119d4589336440be9dc
sagar-git-1 <sagarkushamaz@gmail.com>
2023-09-25 15:59:15 +0530

    Single card functionality updated

--- script.js
+++ script.js
@@ -8,21 +8,19 @@ var singlecard = document.getElementById('singleCard')
 var isssinglecard = false
 
 // show task button
-function showAddTask()
-{
+function showAddTask() {
     addtaskpopup.classList.remove('hide')
     parent.classList.add('blur')
 }
 
 // hide task button
-function hideAddTask()
-{
+function hideAddTask() {
     addtaskpopup.classList.add('hide')
     parent.classList.remove('blur')
 }
 
 //add card function
-function addCard(){
+function addCard() {
 
     notask.classList.add('hide')
     hideAddTask()
@@ -42,7 +40,7 @@ function addCard(){
     card.appendChild(itemList)
     card.appendChild(addItem)
     card.appendChild(deleteButton)
-    
+
 
     //give value to element
     cardHeading.innerText = cardname.value
@@ -50,21 +48,23 @@ function addCard(){
     card.classList.add('card')
     addItem.innerText = "+"
     deleteButton.innerText = "\u{1F5D1}";
-    
+
     // delete button logic
     deleteButton.addEventListener('click', () => {
         card.remove()
-        if(cardcontainer.innerText === '')
-        notask.classList.remove('hide')
+        if (cardcontainer.innerText === '')
+            notask.classList.remove('hide')
     })
 
     // add item   
-    addItem.addEventListener('click', () => additemfunction(itemList)) 
+    addItem.addEventListener('click', () => additemfunction(itemList))
+    // console.log("additm") 
 
     //from here..........
 
 
     cardHeading.addEventListener('click', () => {
+
         isssinglecard = true
         singlecard.classList.remove('hide')
         cardcontainer.classList.add('hide')
@@ -72,105 +72,155 @@ function addCard(){
         singlecard.classList.add('cardcontainerdisplay')
         let copycard = card.cloneNode(true)
         singlecard.appendChild(copycard)
-        copycard.lastElementChild.addEventListener("click", ()=>{
+        copycard.lastElementChild.addEventListener("click", () => {
             card.remove()
             copycard.remove()
-            if(cardcontainer.innerText === '')
-            notask.classList.remove('hide')
+            if (cardcontainer.innerText === '')
+                notask.classList.remove('hide')
         })
-        const copyaddbutton = copycard.querySelector('button')
-        const copylist = copycard.querySelector("div")
-        copyaddbutton.addEventListener('click', ()=>additemfunction(itemList, copylist))
-        parent.firstElementChild.classList.remove('hide')
-    })
-    
-}
+        let markdoneclone = copycard.querySelectorAll('.markdonebutton')
+        // let itemtextclone = itemclone.querySelector('.span1')
+        console.log(copycard,markdoneclone)
 
-function additemfunction(itemList, copylist)
-{
-        
-        addItemPopup.classList.remove('hide')
-        parent.classList.add('blur')
+        for (let i = 0; i < markdoneclone.length; i++) {
+            markdoneclone[i].addEventListener('click', () => {
 
-        //create items
-        let itemPopup = document.createElement('div')
-        let itemPopupheading = document.createElement("h3")
-        let itemName = document.createElement('input')
-        let addButton = document.createElement('button')
-        let closeButton = document.createElement('button')
+                markdoneclone[i].classList.add('hide')
+                markdoneclone[i].previousSibling.classList.add('line')
 
-        //append element
-        addItemPopup.appendChild(itemPopup)
-        addItemPopup.appendChild(itemPopupheading)
-        addItemPopup.appendChild(itemName)
-        addItemPopup.appendChild(addButton)
-        addItemPopup.appendChild(closeButton)
 
-        //give element values
-        itemPopupheading.innerText = "Add task"
-        addButton.innerText = "Add"
-        closeButton.innerText = "Close"
-
-        addButton.addEventListener('click', () => {
-            
-            addItemPopup.classList.add('hide')
-            parent.classList.remove('blur')
-
-            //creating item element
-            let item = document.createElement('div')
-            let itemtext = document.createElement('span')
-            let markdone = document.createElement('button')
-
-            //append
-            item.appendChild(itemtext)
-            item.appendChild(markdone)
-
-            //value
-            itemtext.innerText = itemName.value
-            markdone.innerText = "Mark done"
-            markdone.style.fontSize = "10px"
-            markdone.style.padding = "2px"
-            markdone.style.backgroundColor = "blue"
-            markdone.style.border = "1px solid blue"
-
-            if(isssinglecard){
-                var copyitem = item.cloneNode(true)
-                copylist.appendChild(copyitem)
-
-            }
-
-            markdone.addEventListener('click', () => {
-                // itemtext.style.color = "blue;"
-                markdone.classList.add('hide')
-                itemtext.classList.add('line')
-
-                if(isssinglecard){
-                    copyitem.lastElementChild.style.textDecoration = "line-through"
-                }
             })
-            
-            //appending the item to item list
-            itemList.appendChild(item)
 
-            addItemPopup.innerText = ""
-        })
+        }
+        const copyaddbutton = copycard.lastElementChild.previousSibling;
+        const copylist = copycard.querySelector("div")
+        copyaddbutton.addEventListener('click', () => additemfunction(itemList, copylist))
+        parent.firstElementChild.classList.remove('hide')
 
-          //close button logic
-          closeButton.addEventListener('click', () => {
-            addItemPopup.classList.add('hide')
-            parent.classList.remove('blur')
+    })
 
-            addItemPopup.innerText = ""
-        })
+
+    // if (isssinglecard) {
+    //     // copyitem.firstElementChild.style.textDecoration = "line-through"
+    //     markdone.addEventListener('click', () => {
+    //         // itemtext.style.color = "blue;"
+    //         markdone.classList.add('hide')
+    //         itemtext.classList.add('line')
+    //     })
+    // }
 
 }
 
-function back()
-{
+function additemfunction(itemList, copylist) {
+
+    addItemPopup.classList.remove('hide')
+    parent.classList.add('blur')
+
+    //create items
+    let itemPopup = document.createElement('div')
+    let itemPopupheading = document.createElement("h3")
+    let itemName = document.createElement('input')
+    let addButton = document.createElement('button')
+    let closeButton = document.createElement('button')
+
+    //append element
+    addItemPopup.appendChild(itemPopup)
+    addItemPopup.appendChild(itemPopupheading)
+    addItemPopup.appendChild(itemName)
+    addItemPopup.appendChild(addButton)
+    addItemPopup.appendChild(closeButton)
+
+    //give element values
+    itemPopupheading.innerText = "Add task"
+    addButton.innerText = "Add"
+    closeButton.innerText = "Close"
+
+    addButton.addEventListener('click', () => {
+        // console.log("nmeeeee")
+        addItemPopup.classList.add('hide')
+        parent.classList.remove('blur')
+
+        //creating item element
+        var item = document.createElement('div')
+        var itemtext = document.createElement('span')
+        var markdone = document.createElement('button')
+
+        //append
+        item.appendChild(itemtext)
+        item.appendChild(markdone)
+
+        //value
+        itemtext.innerText = itemName.value
+        markdone.innerText = "Mark done"
+        markdone.style.fontSize = "10px"
+        markdone.style.padding = "2px"
+        markdone.style.backgroundColor = "blue"
+        markdone.style.border = "1px solid blue"
+
+        markdone.classList.add('markdonebutton')
+        itemtext.classList.add('spanA')
+
+        if (isssinglecard) {
+            // let copylist = copycard.querySelector('div')
+            var copyitem = item.cloneNode(true)
+            copylist.appendChild(copyitem)
+
+            let markdoneclone = copyitem.querySelector('.markdonebutton')
+            let itemtextclone = copyitem.querySelector('.spanA')
+
+            markdoneclone.addEventListener('click', () => {
+                itemtextclone.style.color = "blue;"
+                        markdoneclone.classList.add('hide')
+                        itemtextclone.classList.add('line')
+            })
+
+
+
+           
+        }
+
+        markdone.addEventListener('click', () => {
+            // itemtext.style.color = "blue;"
+            markdone.classList.add('hide')
+            itemtext.classList.add('line')
+
+
+            // if(isssinglecard){
+            //     copyitem.firstElementChild.classList.add('line')
+            // }
+        })
+
+        // if(isssinglecard){
+        //     // copyitem.firstElementChild.style.textDecoration = "line-through"
+        //     markdone.addEventListener('click', () => {
+        //         // itemtext.style.color = "blue;"
+        //         markdone.classList.add('hide')
+        //         itemtext.classList.add('line')
+        //     })
+        // }
+
+        //appending the item to item list
+        itemList.appendChild(item)
+
+        addItemPopup.innerText = ""
+    })
+
+    //close button logic
+    closeButton.addEventListener('click', () => {
+        addItemPopup.classList.add('hide')
+        parent.classList.remove('blur')
+
+        addItemPopup.innerText = ""
+    })
+
+}
+
+function back() {
     isssinglecard = false
     parent.firstElementChild.classList.add('hide')
     singlecard.classList.add('hide')
     cardcontainer.classList.remove('hide')
+    // cardcontainer.appendChild(singlecard.firstElementChild)
     cardcontainer.classList.add('cardcontainerdisplay')
     singlecard.classList.remove('cardcontainerdisplay')
     // cardcontainer.appendChild(singlecard.firstElementChild)
